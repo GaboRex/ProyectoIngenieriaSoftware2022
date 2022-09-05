502ea4c7c029cf2368cc03f102bd5aed8a0a78de
GaboRex <gaboneme11@gmail.com>
2022-09-05 08:18:32 -0400

    Add proyect

--- src/App.js
+++ src/App.js
@@ -14,7 +14,8 @@ function App() {
     getTrainerId();
   }, []);
 
-  return <>{trainer && <PokeCard idPokemon={trainer.id_pokemon}></PokeCard>}</>;
+  //return <>{trainer && <PokeCard idPokemon={trainer.id_pokemon}></PokeCard>}</>;
+  return <><h1>Estoy roto</h1></>;
 }
 
 export default App;
